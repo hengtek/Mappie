1aab5f88b20b641850eebbbc119552f7a3a5a273
stacktraced <timing@crepe.moe>
2025-04-06 15:34:58 +0700

    feat: delegates for casclib

--- Utils/NativeLib.cs
+++ Utils/NativeLib.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,5 +23,28 @@ namespace Mappie.Utils
             byte* scratchBuff,
             ulong scratchSize,
             int threadPhase);
+
+
+        [DllImport("CascLib.dll")]
+        public static extern unsafe bool CascOpenStorage([MarshalAs(UnmanagedType.LPTStr)] string szParams, uint dwLocaleMask, out nint phStorage);
+
+        [DllImport("CascLib.dll")]
+        public static extern unsafe bool CascCloseStorage(nint hStorage);
+
+        [DllImport("CascLib.dll")]
+        public static extern unsafe bool CascCloseFile(nint hFile);
+
+        [DllImport("CascLib.dll")]
+        public static extern unsafe bool CascOpenFile(nint hStorage, [MarshalAs(UnmanagedType.LPTStr)] string szFileName, uint dwLocaleFlags, uint dwOpenFlags, out nint phFile);
+
+        [DllImport("CascLib.dll")]
+        public static extern unsafe nint CascFindFirstFile(nint hStorage, [MarshalAs(UnmanagedType.LPTStr)] string szMask, out nint phFindData, [MarshalAs(UnmanagedType.LPTStr)] string szListFile);
+
+        [DllImport("CascLib.dll")]
+        public static extern unsafe bool CascFindNextFile(nint hFindData, out nint phFindData);
+
+        [DllImport("CascLib.dll")]
+        public static extern unsafe bool CascReadFile(nint hFile, void* lpBuffer, uint dwToRead, out uint pdwRead);
+
     }
 }
